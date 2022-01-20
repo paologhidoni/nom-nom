cfc8d49386291b0d1512aae1af2fcecd51f3a605
Orian <77785778+OrianP@users.noreply.github.com>
2022-01-20 12:02:41 +0000

    Create test for submitting form on homepage
    Co-authored-by: Paolo <15671939+paologhidoni@users.noreply.github.com>
    Co-authored-by: Juliette <53294140+julietteorpen@users.noreply.github.com>

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -10,3 +10,18 @@ it("can navigate to homepage", () => {
     cy.visit('/');
     cy.url().should("include", "/");
 });
+
+// user can submit data
+it("user can submit form", () => {
+  cy.visit('/add-review');
+  cy.get(input['name="user"']).type('Mario');
+  cy.get(input['name="restaurant"']).type('Toadland');
+  cy.get(textarea['name="body"']).type('I love toadland!');
+  cy.get(input['name="rating"']).type(5);
+  cy.get(button['type="submit"']).click();
+
+  cy.get('li').contains('Mario');
+  cy.get('li').contains('Toadland');
+  cy.get('li').contains('I love toadland!');
+  cy.get('li').contains('5');
+})
