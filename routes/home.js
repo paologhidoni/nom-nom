c6215ea343bbc91bb5601109384d00235360d7fa
Paolo <15671939+paologhidoni@users.noreply.github.com>
2022-01-20 17:55:47 +0000

    final accessibility checks

--- routes/home.js
+++ routes/home.js
@@ -22,7 +22,7 @@ function get(request, response) {
     
                 <div class="post__restaurant">
                     <h2 class="post__restaurant--name">${review.restaurant}</h2>
-                    <div class="restaurant--rating">${ratingConverter(review.rating)}</div>
+                    <div class="restaurant__rating">${ratingConverter(review.rating)}</div>
                 </div>
                 
                 <h3 class="post__author">${review.username}</h3>
